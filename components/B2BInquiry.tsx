1cb9613c0c640371b44fc1e9544169e5a6a0d6a7
KAVYAAG07 <3skchocolates@gmail.com>
2026-08-09 17:42:35 +0530

    Create B2BInquiry.tsx

---
+++ components/B2BInquiry.tsx
@@ -0,0 +1,8 @@
+export default function B2BInquiry() {
+  return (
+    <section>
+      <h2>B2B & Corporate Inquiry</h2>
+      <p>Contact us for corporate and bulk chocolate orders.</p>
+    </section>
+  );
+}
