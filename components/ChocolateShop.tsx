d93e3bf575ae8cfa9b8ae0b8db4ee34fdd9a7412
KAVYAAG07 <3skchocolates@gmail.com>
2026-08-09 17:38:23 +0530

    Create ChocolateShop.tsx

---
+++ components/ChocolateShop.tsx
@@ -0,0 +1,8 @@
+export default function ChocolateShop() {
+  return (
+    <section>
+      <h2>Our Chocolate Shop</h2>
+      <p>Explore our delicious chocolate collection.</p>
+    </section>
+  );
+}
