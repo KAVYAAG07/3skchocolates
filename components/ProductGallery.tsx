61d71bf81a24d86d6ac01fe7dd92db99ed98bc8e
KAVYAAG07 <3skchocolates@gmail.com>
2026-08-09 17:32:26 +0530

    Create ProductGallery.tsx

---
+++ components/ProductGallery.tsx
@@ -0,0 +1,8 @@
+export default function ProductGallery() {
+  return (
+    <section>
+      <h2>Our Chocolates</h2>
+      <p>Explore our delicious chocolate collection.</p>
+    </section>
+  );
+}
